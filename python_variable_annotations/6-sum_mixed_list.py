ed232545db878f4ed25d98836b8c2938d65a0342
tkeeching <tkeeching@gmail.com>
2024-04-18 15:30:47 +1000

    Test

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -18,8 +18,8 @@ def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
 
 
 # Tests
-print(sum_mixed_list.__annotations__)
-mixed = [5, 4, 3.14, 666, 0.99]
-ans = sum_mixed_list(mixed)
-print(ans == sum(mixed))
-print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
+# print(sum_mixed_list.__annotations__)
+# mixed = [5, 4, 3.14, 666, 0.99]
+# ans = sum_mixed_list(mixed)
+# print(ans == sum(mixed))
+# print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
