313f6a8a7165013f10c7aeae384fd0dba37780c9
tkeeching <tkeeching@gmail.com>
2024-04-18 10:44:06 +1000

    Document

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
+"""
+A type-annotated function that takes two
+float arguments and returns their sums
+as a float
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Returns the sum as a float
+    """
     return a + b
 
 
