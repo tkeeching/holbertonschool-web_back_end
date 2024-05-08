2d97cdc0b72fb885d02171b0d3f511777ea1e9f7
tkeeching <tkeeching@gmail.com>
2024-05-08 16:30:18 +1000

    Fix indent

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Run time for four parallel comprehensions
+"""
+
 import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -14,11 +18,10 @@ async def measure_runtime() -> float:
     start_time = asyncio.get_event_loop().time()  # Record start time
 
     # Execute async_comprehension four times in parallel using asyncio.gather
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension())
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
 
     end_time = asyncio.get_event_loop().time()  # Record end time
 
