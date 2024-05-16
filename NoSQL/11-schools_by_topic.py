6d39a125644c4c91a95e95b61665fcd9486299c9
tkeeching <tkeeching@gmail.com>
2024-05-16 15:30:46 +1000

    Where can I learn Python

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""Returns the list of school having a specific topic"""
+
+from pymongo import MongoClient
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Returns the list of school having the specified topic"""
+
+    document = list(mongo_collection.find({"topics": topic}))
+
+    return document
+
+
+# Tests
+# list_all = __import__('8-all').list_all
+# insert_school = __import__('9-insert_school').insert_school
+
+# if __name__ == "__main__":
+#     client = MongoClient('mongodb://127.0.0.1:27017')
+#     school_collection = client.local.school
+
+#     j_schools = [
+#         {'name': "Holberton school",
+#          'topics': ["Algo", "C", "Python", "React"]},
+#         {'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
+#         {'name': "UCLA", 'topics': ["C", "Python"]},
+#         {'name': "UCSD", 'topics': ["Cassandra"]},
+#         {'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
+#     ]
+#     for j_school in j_schools:
+#         insert_school(school_collection, **j_school)
+
+#     schools = schools_by_topic(school_collection, "Python")
+#     for school in schools:
+#         print("[{}] {} {}".format(
+#             school.get('_id'),
+#             school.get('name'),
+#             school.get('topics', "")))
