55f21c8f84af334efce72cefb90391783132195c
tkeeching <tkeeching@gmail.com>
2024-05-16 14:43:45 +1000

    Test

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -18,7 +18,7 @@ def insert_school(mongo_collection, **kwargs):
 
 # if __name__ == "__main__":
 #     client = MongoClient('mongodb://127.0.0.1:27017')
-#     school_collection = client.my_db.school
+#     school_collection = client.local.school
 #     new_school_id = insert_school(
 #         school_collection, name="UCSF", address="505 Parnassus Ave")
 #     print("New school created: {}".format(new_school_id))
