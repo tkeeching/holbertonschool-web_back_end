a3f11286149e1429a24036b46de6f8fc0604935c
tkeeching <tkeeching@gmail.com>
2024-05-23 13:29:49 +1000

    Fix style

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -45,11 +45,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(
-        self,
-        page: int = 1,
-        page_size: int = 10
-    ) -> List[List]:
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         Retrieve specified page
         """
