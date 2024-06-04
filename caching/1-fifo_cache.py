a028022a5d32fdcbf692f6384ad0f71507a32519
tkeeching <tkeeching@gmail.com>
2024-06-04 20:49:32 +1000

    FIFO caching

---
+++ caching/1-fifo_cache.py
@@ -0,0 +1,63 @@
+#!/usr/bin env python3
+"""
+FIFOCache module that inherits from BaseCaching module
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache module
+    """
+    def __init__(self):
+        super().__init__()
+        self.queue = []  # List to maintain insertion order
+
+    def put(self, key, item):
+        """
+        - Must assign to the dictionary 'self.cache_data' the 'item'
+          value for the key 'key'.
+        - If 'key' or 'item' is 'None', this method should not do anything.
+        - If the number of items in 'self.cache_data' is higher that
+          'BaseCaching.MAX_ITEMS':
+            - you must discard the first item put in cache (FIFO algorithm)
+            - you must print 'DISCARD:' with the 'key' discarded and
+              following by a new line
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = self.queue.pop(0)
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """
+        - Must return the value in 'self.cache_data' linked to 'key'.
+        - If 'key' is 'None' or if the 'key' doesn’t exist in
+          'self.cache_data', return 'None'.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+
+        return self.cache_data[key]
+
+
+# Tests
+# my_cache = FIFOCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
